6e8848e5cb895c16c6cc5c815401de0f825c12d5
helen <447539789@qq.com>
2019-03-23 22:50:01 +0800

    使用BottomNavigation+Fragment，未完成

--- app/src/main/java/com/example/wyb/anti_abuse/MainActivity.java
+++ app/src/main/java/com/example/wyb/anti_abuse/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.wyb.anti_abuse;
 
+import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -21,7 +30,7 @@ import lecho.lib.hellocharts.model.ValueShape;
 import lecho.lib.hellocharts.model.Viewport;
 import lecho.lib.hellocharts.view.LineChartView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements ViewPager.OnPageChangeListener{
 
     private LineChartView lineChart;
     String[] date = {"10-22", "11-22", "12-22", "1-22", "6-22", "5-23", "5-22", "6-22", "5-23", "5-22"};
@@ -29,22 +38,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private List<PointValue> mPointValues = new ArrayList<PointValue>();
     private List<AxisValue> mAxisXValues = new ArrayList<AxisValue>();
 
+    private UserFragment userFragment = new UserFragment();
+    private CoreFragment coreFragment = new CoreFragment();
+    private SoundFragment soundFragment = new SoundFragment();
+    private HeartFragment heartFragment = new HeartFragment();
+    private BottomNavigationView bottomNavigationView;
+    private ViewPager viewPager;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button core_button = (Button) findViewById(R.id.core_button);
-        Button heart_button = (Button) findViewById(R.id.heart_button);
-        Button sound_button = (Button) findViewById(R.id.sound_button);
-        Button user_button = (Button) findViewById(R.id.user_button);
-        core_button.setOnClickListener(this);
-        sound_button.setOnClickListener(this);
-        user_button.setOnClickListener(this);
-        heart_button.setOnClickListener(this);
-        lineChart = (LineChartView)findViewById(R.id.line_chart);
-        getAxisXLabels();
-        getAxisPoints();
-        initLineChart();
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        //添加viewPager事件监听（很容易忘）
+        viewPager.addOnPageChangeListener(this);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        viewPager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                switch (position) {
+                    case 0:
+                        return coreFragment;
+                    case 1:
+                        return heartFragment;
+                    case 2:
+                        return soundFragment;
+                    case 3:
+                        return userFragment;
+                }
+                return null;
+            }
+
+            @Override
+            public int getCount() {
+                return 4;
+            }
+        });
+
+//        lineChart = (LineChartView)findViewById(R.id.line_chart);
+//        getAxisXLabels();
+//        getAxisPoints();
+//        initLineChart();
+//
+//        RoundImageView user_image(RoundImageView)findViewById(R.id.user_img);
+//        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.user);
+//        user_image.setmBitmap(bitmap);
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            //点击BottomNavigationView的Item项，切换ViewPager页面
+            //menu/navigation.xml里加的android:orderInCategory属性就是下面item.getOrder()取的值
+            viewPager.setCurrentItem(item.getOrder());
+            return true;
+        }
+
+    };
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        //页面滑动的时候，改变BottomNavigationView的Item高亮
+        bottomNavigationView.getMenu().getItem(position).setChecked(true);
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
 
     }
 
@@ -99,23 +168,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         v.left = 0;
         v.right = 7;
         lineChart.setCurrentViewport(v);
-    }
-
-    @Override
-    public void onClick(View v){
-        TextView displayView = (TextView) findViewById(R.id.displayView);
-        switch(v.getId()){
-            case R.id.core_button:
-                break;
-            case R.id.heart_button:
-                displayView.setText("心 率 正 常");
-                break;
-            case R.id.sound_button:
-                displayView.setText("声 音 正 常");
-                break;
-            case R.id.user_button:
-                break;
-        }
     }
 
 
